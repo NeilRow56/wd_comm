dacb8e086160747e0f4940d4c02b0e254e2789b3
NeilRow56 <neilrowland56@gmail.com>
2024-05-10 07:37:27 +0100

    Tweak to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from 'tailwindcss'
-const { fontFamily } = require('tailwindcss/defaultTheme')
+import { fontFamily } from 'tailwindcss/defaultTheme'
 
 const config = {
   darkMode: ['class'],
@@ -19,7 +19,7 @@ const config = {
       },
     },
     extend: {
-      ontFamily: {
+      fontFamily: {
         sans: ['var(--font-sans)', ...fontFamily.sans],
       },
       colors: {
